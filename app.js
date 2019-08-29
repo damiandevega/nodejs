a0b1e84785142030affbeeccd6522a63f0ca6b94
Damian de Vega <damiandevega@gmail.com>
2019-08-29 10:18:54 -0400

    creating a routes file and exporting it

--- app.js
+++ app.js
@@ -1,51 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.write(`
-        <html>
-            <head></head>
-            <body>
-                <header>Welcome to Damians Nodejs Server</header>
-                <title>Enter Message</title>
-                <form action="/message" method="POST">
-                    <input type="text" name="message">
-                    <button type="submit">Send</button>
-                </form>
-            </body>
-        </html>`);
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write(`
-        <html>
-            <head></head>
-            <body>
-                <header>Welcome to Damians Nodejs Server</header>
-            </body>
-        </html>`);
-    res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
