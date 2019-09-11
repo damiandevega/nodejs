565819a27aa6d98380b8ed5a9bd2e03295c18374
Damian de Vega <damiandevega@gmail.com>
2019-09-11 17:03:30 -0400

    using post for /product rather than for all request methods

--- app.js
+++ app.js
@@ -5,17 +5,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
-
 app.use('/add-product', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 })
