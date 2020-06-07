7e5b0fd201ffc2e601f31531a55b154958ca2531
Damian de Vega <damiandevega@gmail.com>
2020-06-07 11:35:31 -0400

    cleaning up repo and adding productsController

---
+++ controllers/products.js
@@ -0,0 +1,27 @@
+const products = [];
+
+exports.getAddProductPage = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    productCSS: true,
+    formsCSS: true,
+    activeAddProduct: true
+  })
+}
+
+exports.postAddProductPage = (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+}
+
+exports.getProducts = (req, res, next) => {
+  res.render('shop', {
+    products: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+}
