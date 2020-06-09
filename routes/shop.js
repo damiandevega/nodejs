b41ed8303b794defc1ed55bc0984ba961aca79ec
Damian de Vega <damiandevega@gmail.com>
2020-06-09 14:48:43 -0400

    adding more routes and views for adding to cart

--- routes/shop.js
+++ routes/shop.js
@@ -9,8 +9,14 @@ router.get('/', shopController.getIndex);
 // /products => GET
 router.get('/products', shopController.getProducts);
 
+// /products/:productId => GET
+router.get('/products/:productId', shopController.getProduct);
+
 // /cart => GET
 router.get('/cart', shopController.getCart);
+
+// /cart => POST
+router.post('/cart', shopController.postCart);
 
 // /orders => GET
 router.get('/orders', shopController.getOrders);
