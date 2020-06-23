4e475134c449cfcf1818082ede39f3c55a11946c
Damian de Vega <damiandevega@gmail.com>
2020-06-23 12:15:28 -0400

    adding mongo db

--- routes/shop.js
+++ routes/shop.js
@@ -1,27 +1,25 @@
 const path = require('path');
+
 const express = require('express');
-const shopController = require('../controllers/shop')
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
-// / => GET
 router.get('/', shopController.getIndex);
 
-// /products => GET
 router.get('/products', shopController.getProducts);
 
-// /products/:productId => GET
-router.get('/products/:productId', shopController.getProduct);
+// router.get('/products/:productId', shopController.getProduct);
 
-// /cart => GET
-router.get('/cart', shopController.getCart);
+// router.get('/cart', shopController.getCart);
 
-// /cart => POST
-router.post('/cart', shopController.postCart);
+// router.post('/cart', shopController.postCart);
 
-// /orders => GET
-router.get('/orders', shopController.getOrders);
+// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-// /checkout => GET
-router.get('/checkout', shopController.getCheckout);
+// router.post('/create-order', shopController.postOrder);
+
+// router.get('/orders', shopController.getOrders);
 
 module.exports = router;
