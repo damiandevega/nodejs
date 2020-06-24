135d8d127fd7835c37333549cf880245b3663dee
Damian de Vega <damiandevega@gmail.com>
2020-06-24 15:36:37 -0400

    fixing add product bug

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = id;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
@@ -16,7 +16,7 @@ class Product {
     if (this._id) {
       // Update existing product
       dbOp = db.collection('products')
-        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+        .updateOne({ _id: this._id }, { $set: this });
     } else {
       // Add new product
       dbOp = db.collection('products')
@@ -53,6 +53,18 @@ class Product {
       .then(product => {
         console.log(product);
         return product;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => {
+        console.log('Deleted');
       })
       .catch(error => {
         console.log(error);
