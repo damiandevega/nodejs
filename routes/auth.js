5d12d9cd93940f07ee0c68b65112927b94095291
Damian de Vega <damiandevega@gmail.com>
2020-07-05 14:13:45 -0400

    adding middleware for prected routes and csrf token

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const router = express.Router();
 
 const authController = require('../controllers/auth');
-
-const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
